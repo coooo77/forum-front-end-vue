4df972c490743bf599a7e70b2f6965b2fd7962fb
TL Chang <coooo77@hotmail.com>
2020-05-19 15:57:08 +0800

    U102 install moment.js and refine time displayed

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -2,36 +2,13 @@
   <div class="card">
     <div class="card-header">最新餐廳</div>
     <div class="card-body">
-      <div>
+      <div v-for="restaurant in restaurants" :key="restaurant.id">
         <h4>
-          <a href="#">Deangelo Farrell</a>
-          <small>中式料理</small>
+          <a href="#">{{restaurant.name}}</a>
+          <small>{{ restaurant.Category.name || '未分類' }}</small>
         </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr />
-      </div>
-      <div>
-        <h4>
-          <a href="#">Carter Howe</a>
-          <small>日本料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr />
-      </div>
-      <div>
-        <h4>
-          <a href="#">Dr. Carole Smith</a>
-          <small>義大利料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr />
-      </div>
-      <div>
-        <h4>
-          <a href="#">Nyah Bernier</a>
-          <small>素食料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
+        <p>{{ restaurant.description }}</p>
+        {{ restaurant.createdAt | fromNow}}
         <hr />
       </div>
     </div>
@@ -43,3 +20,25 @@ small {
   font-size: 10px;
 }
 </style>
+
+<script>
+import moment from "moment";
+
+export default {
+  filters: {
+    fromNow(datetime) {
+      if (!datetime) {
+        return "-";
+      }
+      // 使用 moment 提供的 fromNow 方法
+      return moment(datetime).fromNow();
+    }
+  },
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
