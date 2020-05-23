be536bb08aa0b879328aa6db3a6925ce53e625d0
TL Chang <coooo77@hotmail.com>
2020-05-23 22:05:51 +0800

    U132

--- src/components/topUser.vue
+++ src/components/topUser.vue
@@ -4,21 +4,24 @@
       <img :src="topUser.image | emptyImage" width="180px" height="180px" />
     </a>
     <h2>{{user.name}}</h2>
-    <span class="badge badge-secondary">追蹤人數：{{topUser.FollowerCount}}</span>
+    <span class="badge badge-secondary">追蹤人數：{{topUser.followerCount}}</span>
     <p class="mt-3">
       <button
         v-if="topUser.isFollowed"
         type="button"
         class="btn btn-danger"
-        @click.stop.prevent="unFollow"
+        @click.stop.prevent="unFollow(user.id)"
       >取消追蹤</button>
-      <button v-else type="button" class="btn btn-primary" @click.stop.prevent="follow">追蹤</button>
+      <button v-else type="button" class="btn btn-primary" @click.stop.prevent="follow(user.id)">追蹤</button>
     </p>
   </div>
 </template>
 
 <script>
 import { emptyImageFilter } from "./../utils/mixins";
+import usersAPI from "../apis/users";
+import { Toast } from "../utils/helpers";
+
 export default {
   mixins: [emptyImageFilter],
   props: {
@@ -33,20 +36,45 @@ export default {
     };
   },
   methods: {
-    follow() {
-      // 如果我不直接用template，可以直接再UsersTop.vue做出topUsers嗎?
-      this.topUser = {
-        ...this.topUser,
-        FollowerCount: ++this.topUser.FollowerCount,
-        isFollowed: true
-      };
+    async follow(userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.topUser = {
+          ...this.topUser,
+          followerCount: ++this.topUser.followerCount,
+          isFollowed: true
+        };
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法加入追蹤，請稍後再試"
+        });
+      }
     },
-    unFollow() {
-      this.topUser = {
-        ...this.topUser,
-        FollowerCount: --this.topUser.FollowerCount,
-        isFollowed: false
-      };
+    async unFollow(userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
+        }
+
+        this.topUser = {
+          ...this.topUser,
+          followerCount: --this.topUser.followerCount,
+          isFollowed: false
+        };
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取消追蹤，請稍後再試"
+        });
+      }
     }
   }
 };
