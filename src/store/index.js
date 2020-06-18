b07fcddc0d4bd94589bbaa3b997f0583f030a1bd
TL Chang <coooo77@hotmail.com>
2020-06-18 17:14:01 +0800

    U140

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import usersAPI from '../apis/users'
 
 Vue.use(Vuex)
 
@@ -24,6 +25,28 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    // 在 actions 中可以透過參數的方式取得 commit 的方法
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data, status, statusText } = await usersAPI.getCurrentUser()
+
+        if (status !== 200) {
+          throw new Error(statusText)
+        }
+
+        const { id, name, email, image, isAdmin } = data
+
+        commit('setCurrentUser', {
+          id,
+          name,
+          email,
+          image,
+          isAdmin
+        })
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
   },
   modules: {
   }
